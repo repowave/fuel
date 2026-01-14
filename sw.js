ea72ffc5bd0d408ab4e86a26f94bfadf15dc0289
repowave <149286391+repowave@users.noreply.github.com>
2026-01-14 18:43:42 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,93 @@
+// Service Worker for FuelTracker Pro
+const CACHE_NAME = 'fueltracker-v2.0';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png'
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // Clone the request
+        const fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(response => {
+          // Check if we received a valid response
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+
+          // Clone the response
+          const responseToCache = response.clone();
+
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        });
+      })
+      .catch(() => {
+        // If both cache and network fail, show offline page
+        if (event.request.mode === 'navigate') {
+          return caches.match('/');
+        }
+      })
+  );
+});
+
+// Background sync for offline data
+self.addEventListener('sync', event => {
+  if (event.tag === 'sync-entries') {
+    event.waitUntil(syncEntries());
+  }
+});
+
+async function syncEntries() {
+  // Sync logic would go here
+  console.log('Syncing entries...');
+}
